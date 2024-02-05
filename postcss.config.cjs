3f57b600ef9753f0c876faa48db2f58993d1b0ad
shantanu-cleartax <shantanu.mahale@clear.in>
2024-02-05 11:59:25 +0530

    adding tailwind to the project

---
+++ postcss.config.cjs
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+module.exports = {
+  // inject: false,
+  // extract: true,
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+  },
+};
